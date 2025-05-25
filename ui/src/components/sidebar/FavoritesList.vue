1965fd101357d4a7da729d64d636df6ab3126ab1
Jeremy Mouton <jeremy.mouton@spendesk.com>
2025-05-25 02:33:09 +0200

    Add FavoritesList component to Sidebar and implement favorites fetching on mount

---
+++ ui/src/components/sidebar/FavoritesList.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import { onMounted } from 'vue';
+import { useFavoriteStore } from "../../stores/favorite";
+
+// Use defineProps without assigning to a variable
+// const { isCollapsed, isHovered } = defineProps<{
+//   isCollapsed: boolean,
+//   isHovered: boolean
+// }>()
+
+const favoritesStore = useFavoriteStore();
+
+const capitalizeFirst = (str: string | undefined) => {
+  if (!str) return ''
+  return str.charAt(0).toUpperCase() + str.slice(1)
+}
+
+onMounted(async() => {
+  await favoritesStore.fetchFavorites();
+});
+</script>
+
+<template>
+  <div class="pt-2 pb-1 text-[13px]" v-if=favoritesStore.favorites.length>
+    <router-link
+      v-for="favorite in favoritesStore.favorites"
+      :key="favorite.id"
+      :to="`/d/${favorite.document?.slug || ''}`"
+      class="flex items-center gap-x-2 rounded-lg px-2 py-1 text-[13px] text-gray-500 hover:bg-gray-100 hover:text-gray-700">
+      <div class="size-4 rounded bg-gray-100 flex items-center justify-center opacity-75 text-xs font-medium">
+        {{ favorite.document?.name[0]?.toUpperCase() }}
+      </div>
+      <span>{{ capitalizeFirst(favorite.document?.name) }}</span>
+    </router-link>
+  </div>
+</template>
