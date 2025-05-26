c437c0075e7ba625696af39c049b52e0f4590dac
Jeremy Mouton <moutonjeremy@labbs.fr>
2025-05-26 18:31:28 +0200

    Remove unused imports from Vue components (#7)
    * Remove unused import of Directive from Document.vue
    
    * Remove unused import of Transition in ConfigSidebar.vue

--- ui/src/components/document/ConfigSidebar.vue
+++ ui/src/components/document/ConfigSidebar.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts">
 import { computed, ref, watch } from 'vue';
 import { type Document } from '../../api/document';
-import { Transition } from 'vue';
 
 const props = defineProps<{
   visible: boolean;
