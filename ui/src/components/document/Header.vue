d7a77b799285cb36b20130cbd9194d5e67551555
Jeremy Mouton <jeremy.mouton@spendesk.com>
2025-05-26 17:12:05 +0200

    feat(header): refactor to use ConfigSidebar and update icon handling

--- ui/src/components/document/Header.vue
+++ ui/src/components/document/Header.vue
@@ -2,8 +2,8 @@
 import { type Directive, ref } from 'vue';
 import { useFavoriteStore } from '../../stores/favorite';
 import { type Document } from '../../api/document';
-import { FaceSmileIcon, PhotoIcon } from "@heroicons/vue/24/solid";
-import IconSidebar from './IconSidebar.vue';
+import { AdjustmentsHorizontalIcon } from "@heroicons/vue/24/solid";
+import ConfigSidebar from './ConfigSidebar.vue';
 
 const favoritesStore = useFavoriteStore();
 
@@ -20,24 +20,34 @@ const emit = defineEmits<{
   (e: 'update:editableTitle', value: string): void;
   (e: 'setDocumentIcon', icon: string): void;
   (e: 'toggleEditorEditable', editable: boolean): void;
-  (e: 'iconSidebarVisibility', visible: boolean): void;
+  (e: 'iconConfigVisibility', visible: boolean): void;
+  (e: 'toggleLock', lock: boolean): void;
+  (e: 'setFullWidth', fullWidth: boolean): void;
 }>();
 
-// État pour la sidebar d'icônes
-const showIconSidebar = ref(false);
+// État pour la sidebar de configuration
+const showConfigSidebar = ref(false);
 
-// Fonction pour ouvrir/fermer la sidebar d'icônes
-const toggleIconSidebar = () => {
-  showIconSidebar.value = !showIconSidebar.value;
+const toggleConfigSidebar = () => {
+  showConfigSidebar.value = !showConfigSidebar.value;
   // Désactiver l'éditeur quand la sidebar est ouverte
-  emit('toggleEditorEditable', !showIconSidebar.value);
+  emit('toggleEditorEditable', !showConfigSidebar.value);
   // Notifier le parent de la visibilité de la sidebar
-  emit('iconSidebarVisibility', showIconSidebar.value);
+  emit('iconConfigVisibility', showConfigSidebar.value);
 };
 
 // Fonction pour appliquer une icône
 const applyIcon = (icon: string) => {
   emit('setDocumentIcon', icon);
+};
+
+// Fonction pour basculer le verrouillage du document
+const toggleLock = (lock: boolean) => {
+  emit('toggleLock', lock);
+};
+
+const fullWidth = (fullWidth: boolean) => {
+  emit('setFullWidth', fullWidth);
 };
 
 // Directive focus
@@ -48,7 +58,7 @@ const vFocus: Directive = {
 
 <template>
   <div class="sticky top-0 border-b border-e bg-white">
-    <div class="flex justify-between items-center h-24 px-8">
+    <div class="flex justify-between items-center h-16 px-8">
       <div class="flex-1" />
       <div v-if="isEditingTitle" class="flex-1">
         <input 
@@ -66,31 +76,20 @@ const vFocus: Directive = {
         class="flex-1 flex flex-col items-center justify-center relative group"
       >
         <h1 
-          class="text-2xl font-medium text-gray-600 text-center cursor-text flex items-center justify-center"
-          @click="emit('update:isEditingTitle', true)"
+          class="text-2xl font-medium text-gray-600 text-center flex items-center justify-center"
+          @click="!currentDocument?.config?.lock && emit('update:isEditingTitle', true)"
         >
           <span v-if="currentDocument?.config?.icon" class="mr-2">{{ currentDocument.config.icon }}</span>
           <span>{{ currentDocument?.name }}</span>
         </h1>
-        <div class="opacity-0 group-hover:opacity-100 transition-opacity mt-2 flex items-center">
-          <div 
-            class="text-gray-400 cursor-pointer hover:bg-gray-100 hover:text-gray-500 rounded-lg px-1 flex items-center"
-            @click="toggleIconSidebar"
-          >
-            <div class="flex-shrink-0 mr-1">
-              <FaceSmileIcon class="size-4 opacity-75" />
-            </div>
-            <span class="flex-grow">{{ currentDocument?.config?.icon ? 'Change icon' : 'Add an icon' }}</span>
-          </div>
-          <div class="text-gray-400 cursor-pointer hover:bg-gray-100 hover:text-gray-500 rounded-lg px-1 flex items-center ml-2">
-            <div class="flex-shrink-0 mr-1">
-              <PhotoIcon class="size-4 opacity-75" />
-            </div>
-            <span class="flex-grow">Add a background</span>
-          </div>
-        </div>
       </div>
       <div class="flex-1 flex items-center justify-end gap-2">
+        <div v-if="currentDocument?.config?.lock" class="top-0 right-0 m-4 flex items-center px-3 py-1.5 rounded-md bg-red-50 text-red-500 border border-red-200">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4 mr-1.5">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M16.5 10.5V6.75a4.5 4.5 0 10-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 002.25-2.25v-6.75a2.25 2.25 0 00-2.25-2.25H6.75a2.25 2.25 0 00-2.25 2.25v6.75a2.25 2.25 0 002.25 2.25z" />
+          </svg>
+                <span class="text-xs font-medium">Document locked</span>
+        </div>
         <button 
           class="text-gray-400 hover:text-yellow-500 transition-colors p-2 rounded-lg hover:bg-gray-100"
           @click="emit('favorite')"
@@ -102,15 +101,23 @@ const vFocus: Directive = {
             <path stroke-linecap="round" stroke-linejoin="round" d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z"></path>
           </svg>
         </button>
+        <button
+          class="text-gray-400 hover:text-blue-500 transition-colors p-2 rounded-lg hover:bg-gray-100"
+          @click="toggleConfigSidebar"
+          >
+          <AdjustmentsHorizontalIcon class="size-6" />
+        </button>
       </div>
     </div>
     
-    <!-- IconSidebar component -->
-    <IconSidebar 
-      :visible="showIconSidebar" 
+    <!-- Config Sidebar -->
+    <ConfigSidebar 
+      :visible="showConfigSidebar" 
       :currentDocument="currentDocument"
-      @close="toggleIconSidebar"
+      @close="toggleConfigSidebar"
       @setDocumentIcon="applyIcon"
+      @setLock="toggleLock"
+      @setFullWidth="fullWidth"
     />
   </div>
 </template>
