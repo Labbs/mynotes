f9c5fce22cd184a56cfd1a3e7aaf96311fa11bd0
Jeremy Mouton <jeremy.mouton@spendesk.com>
2025-05-30 01:30:01 +0200

    Add search functionality to CommonMenu with MagnifyingGlassIcon

--- ui/src/components/sidebar/CommonMenu.vue
+++ ui/src/components/sidebar/CommonMenu.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { computed } from "vue";
 import { useRouter } from "vue-router";
-import { HomeIcon, Cog6ToothIcon } from "@heroicons/vue/24/outline";
+import { HomeIcon, Cog6ToothIcon, MagnifyingGlassIcon } from "@heroicons/vue/24/outline";
 
 const { isCollapsed, isHovered } = defineProps<{
   isCollapsed: boolean,
@@ -20,6 +20,16 @@ const isSettingsPage = computed(() => {
 
 <template>
   <div class="pt-2 pb-1 text-[13px]">
+    <div       class="flex items-center gap-x-2 rounded-lg px-1 py-1 text-gray-500 hover:bg-gray-100 hover:text-gray-700"
+      :class="{
+        'justify-center': isCollapsed && !isHovered,
+      }"
+    >
+      <div class="flex-shrink-0">
+        <MagnifyingGlassIcon class="size-5 opacity-75" />
+      </div>
+      <span v-show="!isCollapsed || isHovered" class="flex-grow"> Search </span>
+    </div>
     <router-link
       to="/"
       class="flex items-center gap-x-2 rounded-lg px-1 py-1 text-gray-500 hover:bg-gray-100 hover:text-gray-700"
