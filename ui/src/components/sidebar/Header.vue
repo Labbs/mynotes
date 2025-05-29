d2aea6c91e9b7d98f19ea2dc14c73eaebea0c0c5
Jeremy Mouton <jeremy.mouton@spendesk.com>
2025-05-30 01:30:08 +0200

    Update Header component styles for improved layout and readability

--- ui/src/components/sidebar/Header.vue
+++ ui/src/components/sidebar/Header.vue
@@ -6,10 +6,10 @@ const { isCollapsed, isHovered } = defineProps<{
 </script>
 
 <template>
-  <div class="text-[13px]">
+  <div class="pl-2 text-[15px]">
     <router-link to="/" class="flex items-center gap-x-2 text-gray-500">
       <div
-        class="size-5 rounded-lg bg-gray-100 flex items-center justify-center"
+        class="p-4 size-5 rounded-lg bg-gray-100 flex items-center justify-center"
       >
         <span class="font-medium">MN</span>
       </div>
