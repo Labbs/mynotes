8fda59f1b9a93313b830930045ce164ea9b244ed
Jeremy Mouton <jeremy.mouton@spendesk.com>
2025-05-30 12:29:57 +0200

    Remove unused props and simplify template structure in CommonMenu component

--- ui/src/components/sidebar/CommonMenu.vue
+++ ui/src/components/sidebar/CommonMenu.vue
@@ -3,11 +3,6 @@ import { computed } from "vue";
 import { useRouter } from "vue-router";
 import { HomeIcon, Cog6ToothIcon, MagnifyingGlassIcon } from "@heroicons/vue/24/outline";
 
-const { isCollapsed, isHovered } = defineProps<{
-  isCollapsed: boolean,
-  isHovered: boolean
-}>()
-
 const router = useRouter();
 
 const isHomePage = computed(() => {
@@ -20,28 +15,23 @@ const isSettingsPage = computed(() => {
 
 <template>
   <div class="pt-2 pb-1 text-[13px]">
-    <div       class="flex items-center gap-x-2 rounded-lg px-1 py-1 text-gray-500 hover:bg-gray-100 hover:text-gray-700"
-      :class="{
-        'justify-center': isCollapsed && !isHovered,
-      }"
-    >
+    <div       class="flex items-center gap-x-2 rounded-lg px-1 py-1 text-gray-500 hover:bg-gray-100 hover:text-gray-700">
       <div class="flex-shrink-0">
         <MagnifyingGlassIcon class="size-5 opacity-75" />
       </div>
-      <span v-show="!isCollapsed || isHovered" class="flex-grow"> Search </span>
+      <span> Search </span>
     </div>
     <router-link
       to="/"
       class="flex items-center gap-x-2 rounded-lg px-1 py-1 text-gray-500 hover:bg-gray-100 hover:text-gray-700"
       :class="{
         'bg-gray-100 text-gray-700': isHomePage,
-        'justify-center': isCollapsed && !isHovered,
       }"
     >
       <div class="flex-shrink-0">
         <HomeIcon class="size-5 opacity-75" />
       </div>
-      <span v-show="!isCollapsed || isHovered" class="flex-grow"> Home </span>
+      <span class="flex-grow"> Home </span>
     </router-link>
 
     <router-link
@@ -49,13 +39,12 @@ const isSettingsPage = computed(() => {
       class="flex items-center gap-x-2 rounded-lg px-1 py-1 text-gray-500 hover:bg-gray-100 hover:text-gray-700 mt-1"
       :class="{
         'bg-gray-50 text-gray-700': isSettingsPage,
-        'justify-center': isCollapsed && !isHovered,
       }"
     >
       <div class="flex-shrink-0">
         <Cog6ToothIcon class="size-5 opacity-75" />
       </div>
-      <span v-show="!isCollapsed || isHovered" class="flex-grow">
+      <span class="flex-grow">
         Settings
       </span>
     </router-link>
