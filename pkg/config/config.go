3cea974920622b709c500e9b6e6e058c59e02eab
Jeremy Mouton <jeremy.mouton@spendesk.com>
2025-06-07 22:19:02 +0200

    feat: Add Cache configuration struct with Redis and Memory options

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -27,4 +27,20 @@ var (
 	Document struct {
 		ExcalidrawLibsPath string
 	}
+
+	Cache struct {
+		Type   string // Cache type (memory, redis, etc.)
+		Expire int    // Cache expiration time in seconds
+		Redis  struct {
+			Addr     string // Redis server address
+			Password string // Redis password
+			DB       int    // Redis database number
+		}
+
+		Memory struct {
+			Size int // Memory cache size in MB
+		}
+
+		Enable bool // Enable or disable caching
+	}
 )
