45c5144e85358211e260a7d3366924df6f5d8918
Jeremy Mouton <jeremy.mouton@spendesk.com>
2025-06-07 22:17:05 +0200

    feat: Implement RBAC check logic in middleware

--- pkg/api/middleware/rbac_check.go
+++ pkg/api/middleware/rbac_check.go
@@ -1,13 +1,35 @@
 package middleware
 
 import (
+	"fmt"
+
 	"github.com/gofiber/fiber/v2"
+	"github.com/labbs/mynotes/pkg/models"
 	"github.com/rs/zerolog"
 )
 
-func RBACCheckMiddleware(logger zerolog.Logger) fiber.Handler {
+func RBACCheckMiddleware(logger zerolog.Logger, userService models.UserService, groupService models.GroupService, spaceService models.SpaceService, documentService models.DocumentService) fiber.Handler {
 	return func(c *fiber.Ctx) error {
-		// TODO: Implement RBAC check logic here
+		userId := c.Locals("userId").(string)
+		groups, err := userService.GetGroupsByUserId(userId)
+		if err != nil {
+			logger.Error().Err(err).Msg("Failed to get user groups")
+			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+				"error": "Failed to get user groups",
+			})
+		}
+
+		// check if one of the groups is an admin group
+		for _, group := range groups {
+			if group.Role == models.RoleAdmin {
+				// User is an admin, allow access
+				return c.Next()
+			}
+		}
+
+		// check if the path
+
+		fmt.Println("Checking RBAC for path:", c.Path())
 
 		return c.Next()
 	}
