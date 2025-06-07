73043b8a9c4d3dd8b74a729f02d77054e78d8511
Jeremy Mouton <jeremy.mouton@spendesk.com>
2025-06-07 22:18:42 +0200

    feat: Define Member, Members, MemberType, and AccessType models with associated methods

--- pkg/models/common.go
+++ pkg/models/common.go
@@ -19,3 +19,41 @@ func (j JSONB) Value() (driver.Value, error) {
 func (j *JSONB) Scan(value any) error {
 	return json.Unmarshal([]byte(value.(string)), j)
 }
+
+// MemberType is the type of member
+type MemberType string
+
+// AccessType is the access type of a member
+type AccessType string
+
+type Members []Member
+
+func (m Members) Value() (driver.Value, error) {
+	valueString, err := json.Marshal(m)
+	return string(valueString), err
+}
+
+func (m *Members) Scan(value any) error {
+	return json.Unmarshal([]byte(value.(string)), m)
+}
+
+// Member is a model for a member
+type Member struct {
+	Id     string     `json:"id"`
+	Type   MemberType `json:"type"`
+	Access AccessType `json:"access"`
+}
+
+// AccessTypeViewer is the access type viewer
+const (
+	AccessTypeViewer  AccessType = "viewer"
+	AccessTypeEditor  AccessType = "editor"
+	AccessTypeComment AccessType = "comment"
+	AccessTypeFull    AccessType = "full"
+)
+
+// MemberType is the type of member
+const (
+	MemberTypeUser  MemberType = "user"
+	MemberTypeGroup MemberType = "group"
+)
