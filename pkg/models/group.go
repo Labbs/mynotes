817914271ddfd4ef8104b038380b4874c1aa49b6
Jeremy Mouton <jeremy.mouton@spendesk.com>
2025-06-07 22:18:31 +0200

    feat: Add RoleType to Group model and update GetWithUsers method for consistency

--- pkg/models/group.go
+++ pkg/models/group.go
@@ -8,9 +8,10 @@ import (
 )
 
 type Group struct {
-	Id          string `json:"id"`
-	Name        string `json:"name"`
-	Description string `json:"description"`
+	Id          string   `json:"id"`
+	Name        string   `json:"name"`
+	Description string   `json:"description"`
+	Role        RoleType `json:"role"` // e.g., "admin", "user", etc.
 
 	Users []User `json:"users" gorm:"many2many:user_group;"`
 
@@ -32,11 +33,19 @@ func (g *Group) BeforeUpdate(tx *gorm.DB) error {
 	return nil
 }
 
+type RoleType string
+
+const (
+	RoleAdmin RoleType = "admin"
+	RoleUser  RoleType = "user"
+	RoleGuest RoleType = "guest"
+)
+
 // GroupRepository is the repository for groups
 type GroupRepository interface {
 	Create(group Group) (Group, error)
 	GetById(id string) (Group, error)
-	GetWithUsers(id string) (Group, error)
+	GetGroupWithUsers(id string) (Group, error)
 	GetAll() ([]Group, error)
 	Update(group Group) (Group, error)
 	Delete(id string) error
