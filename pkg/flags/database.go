4943eb7baf8103e767e8dec0240d2b8848916bbe
Jeremy Mouton <jeremy.mouton@spendesk.com>
2025-07-12 00:44:35 +0200

    refactor: simplify database flag names by removing gorm prefix

--- pkg/flags/database.go
+++ pkg/flags/database.go
@@ -9,17 +9,17 @@ import (
 func DatabaseFlags() []cli.Flag {
 	return []cli.Flag{
 		altsrc.NewStringFlag(&cli.StringFlag{
-			Name:        "database.gorm.dsn",
-			Aliases:     []string{"dgd"},
-			EnvVars:     []string{"DATABASE_GORM_DSN"},
+			Name:        "database.dsn",
+			Aliases:     []string{"dsn"},
+			EnvVars:     []string{"DATABASE_DSN"},
 			Usage:       "Database Gorm DSN",
 			Value:       "./database.db",
 			Destination: &config.Database.DSN,
 		}),
 		altsrc.NewStringFlag(&cli.StringFlag{
-			Name:        "database.gorm.dialect",
-			Aliases:     []string{"dgdialect"},
-			EnvVars:     []string{"DATABASE_GORM_DIALECT"},
+			Name:        "database.dialect",
+			Aliases:     []string{"dialect"},
+			EnvVars:     []string{"DATABASE_DIALECT"},
 			Usage:       "Database Gorm Dialect",
 			Value:       "sqlite",
 			Destination: &config.Database.Dialect,
